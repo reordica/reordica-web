c35259f01cf0f14575e729cc8490f32937fec5b4
Garry Ledwith <garryledwith@gmail.com>
2026-04-05 06:56:26 +0100

    placed vercel speed insights component in app root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <body style={{ fontFamily: "Arial, Helvetica, sans-serif" }}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
